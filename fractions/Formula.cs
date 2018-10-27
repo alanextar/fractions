6d108da5cee9f7f778bbee08ab2622221e970f10
Aleksandr Tarasov <alanextar@gmail.com>
2018-10-27 18:29:58 +0500

    Created two classes : Formula and Fraction. Described them partially.

---
+++ fractions/Formula.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fractions
+{
+    class Formula
+    {
+        public enum OpType {
+            Sum,
+            Subtraction,
+            Dividing,
+            Multiplicaton
+        }
+        
+        public double MathOperation(Fractions fraction1, OpType opType, Fractions fraction2)
+        {
+            if(opType.Equals("Sum"))
+            {
+                return fraction1.Sum(fraction2);
+            }
+            return 1.0;
+        }
+    }
+}
---
+++ fractions/Fractions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fractions
+{
+    class Fractions
+    {
+        public double Numerator { get; set; }
+        public double Denominator { get; set; }
+
+        public double Sum(double number)
+        {
+            return 1.0;
+        }
+
+        public double Sum(Fractions fraction)
+        {
+            return 1.0;
+        }
+
+        public double Subtraction(double number)
+        {
+            return 1.0;
+        }
+
+        public double Multiplication(double number)
+        {
+            return 1.0;
+        }
+
+        public double Dividing(double number)
+        {
+            return 1.0;
+        }
+    }
+}
